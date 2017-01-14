3145f3a242c075b4040633152c905f03e6c38df4
Zen1124 <KyleBulkiski@Kyles-MacBook-Air.local>
2017-01-13 19:17:46 -0500

    Added Work from week 2

---
+++ README.md
@@ -0,0 +1,2 @@
+# Winter_Boot_Camp_Assignments
+Repo for the Winter Boot Camp or whatever we call it
---
+++ Test/Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Kyle Bulkiski on 1/13/17.
+//  Copyright © 2017 Kyle Bulkiski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
